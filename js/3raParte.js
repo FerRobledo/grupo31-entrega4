20a4f5f795fe2e92dd4bb5aec21e73d5f9b17804
gonza <gonzagiacomino18@gmail.com>
2024-11-15 19:06:28 -0300

    Realizando parte 3 y 4

---
+++ js/3raParte.js
@@ -0,0 +1,18 @@
+const contenedor = document.querySelector('.terceraParte');
+        const image = document.getElementById('3raParte-muniecos');
+        const movementFactor = 0.008; // Factor para reducir el movimiento
+
+        document.addEventListener('mousemove', (event) => {
+            const rect = contenedor.getBoundingClientRect();
+
+            const mouseX = event.clientX - rect.left; 
+            const mouseY = event.clientY - rect.top;
+
+            const containerWidth = rect.width;
+            const containerHeight = rect.height;
+
+            const offsetX = (containerWidth / 2 - mouseX) * movementFactor;
+            const offsetY = (containerHeight / 2 - mouseY) * movementFactor;
+
+            image.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+        });
---
+++ js/4taParte.js
@@ -0,0 +1,11 @@
+const imagenes = [
+    'images/0.png',
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+    'images/6.png',
+    'images/7.png',
+    'images/8.png',
+    'images/9.png',
+    'images/10.png',
+];
